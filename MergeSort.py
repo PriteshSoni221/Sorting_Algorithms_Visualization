d028f485e83c27135846883da3f756cba4d219b0
Pritesh Soni <55101088+PriteshSoni221@users.noreply.github.com>
2020-07-17 20:39:12 +0530

    GUI changed and MergeSort implemented

--- MergeSort.py
+++ MergeSort.py
@@ -1,1 +1,71 @@
 import time
+
+
+def merge_sort(data, draw_data, animation_time):
+    merge_sort_algorithm(data, 0, len(data)-1, draw_data, animation_time)
+    # len(data)-1 because index starts at 0.
+
+
+def merge_sort_algorithm(data, left, right, draw_data, animation_time):
+    if left < right:
+        middle = (left+right)//2
+        # recursion: first left part then right part
+        merge_sort_algorithm(data, left, middle, draw_data, animation_time)
+        merge_sort_algorithm(data, middle+1, right, draw_data, animation_time)
+        merge(data, left, middle, right, draw_data, animation_time)
+
+
+def merge(data, left, middle, right, draw_data, animation_time):
+
+    draw_data(data, get_color_list(len(data), left, middle, right))
+    time.sleep(animation_time)
+
+    left_part = data[left:middle+1]  # middle+1 because we want to include it
+    right_part = data[middle+1:right+1]
+
+    left_index = right_index = 0
+
+    for data_index in range(left, right+1):
+        if left_index < len(left_part) and right_index < len(right_part):
+            # if first element of left part is greater than right one then add first
+            # element of left part else right part
+            if left_part[left_index] <= right_part[right_index]:
+                data[data_index] = left_part[left_index]
+                left_index += 1
+            else:
+                data[data_index] = right_part[right_index]
+                right_index += 1
+        elif left_index < len(left_part):
+            # If there is still some elements in the left part then we want to
+            # simply add them into our sorted data
+            data[data_index] = left_part[left_index]
+            left_index += 1
+        else:
+            # Or if there is still some elements in the right part then we want to
+            # add them into our sorted data
+            data[data_index] = right_part[right_index]
+            right_index += 1
+
+    draw_data(data, ["green" if x >= left and x <=
+                     right else "white" for x in range(len(data))])
+    time.sleep(animation_time)
+
+
+def get_color_list(length, left, middle, right):
+    color_list = []
+
+    for i in range(length):
+        if i >= left and i <= middle:
+            if i >= left and i <= middle:
+                color_list.append("yellow")
+            else:
+                color_list.append("pink")
+        else:
+            color_list.append("white")
+
+    return color_list
+
+
+# data = [1, 5, 2, 5, 3, 2, 4]
+# merge_sort(data, 0, 0)
+# print(data)
--- canvas_experiment.py
+++ canvas_experiment.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import random
 from PIL import Image, ImageTk
 from BubbleSort import bubble_sort  # importing bubble_sort function
+from MergeSort import merge_sort
 
 # variables
 min_height, min_width = 1200, 700
@@ -76,7 +77,7 @@ def generate_arrays():
 
 def start_algorithm():
     global data
-    selectedAlgo = algo_list.get("anchor")
+    selected_algo = algo_list.get("anchor")
     # print("Selected Algorithm "+selectedAlgo)
     # print(data)
     # bubble_sort(data, draw_data, speed_scale.get())
@@ -84,21 +85,21 @@ def start_algorithm():
     if not data:
         return
 
-    if selectedAlgo == 'Bubble Sort':
+    if selected_algo == 'Bubble Sort':
         bubble_sort(data, draw_data, speed_scale.get())
 
-    # elif selectedAlgo == 'Quick Sort':
+    # elif selected_algo == 'Quick Sort':
     #     quick_sort(data, 0, len(data)-1, drawData, speedScale.get())
     #     drawData(data, ['green' for x in range(len(data))])
     #
-    # elif selectedAlgo == 'Merge Sort':
-    #     merge_sort(data, drawData, speedScale.get())
+    elif selected_algo == 'Merge Sort':
+        merge_sort(data, draw_data, speed_scale.get())
     #
-    # elif selectedAlgo == 'insertion Sort':
-    #     insertion_sort(data, drawData, speedScale.get())
+    # elif selected_algo == 'insertion Sort':
+    #     insertion_sort(data, draw_data, speed_scale.get())
     #
-    # elif selectedAlgo == 'selection Sort':
-    #     selection_sort(data, drawData, speedScale.get())
+    # elif selected_algo == 'selection Sort':
+    #     selection_sort(data, draw_data, speed_scale.get())
 
 
 # initializing root
@@ -135,23 +136,23 @@ canvas.place(relx=0.1, rely=0.3, relwidth=0.55, relheight=0.6)
 num_of_elements_scale = tk.Scale(upper_frame, from_=3, to=30,
                                  orient="horizontal", label="Total Elements", troughcolor="#4A5759", bg="#E76F51",
                                  font=("Plantagenet Cherokee", 16))
-num_of_elements_scale.place(relx=0.05, rely=0.2, width=150, height=60)
+num_of_elements_scale.place(relx=0.05, rely=0.15, relwidth=0.2, relheight=0.7)
 
 min_num_scale = tk.Scale(upper_frame, from_=0, to=10,
                          orient="horizontal", label="Minimum Number", troughcolor="#4A5759", bg="#F7E1D7",
                          font=("Plantagenet Cherokee", 16))
-min_num_scale.place(relx=0.34, rely=0.2, width=150, height=60)
+min_num_scale.place(relx=0.34, rely=0.15, relwidth=0.2, relheight=0.7)
 
 max_num_scale = tk.Scale(upper_frame, from_=10, to=100,
                          orient="horizontal", label="Maximum Number", troughcolor="#4A5759", bg="#AFC97E",
                          font=("Plantagenet Cherokee", 16))
-max_num_scale.place(relx=0.63, rely=0.2, width=150, height=60)
+max_num_scale.place(relx=0.63, rely=0.15, relwidth=0.2, relheight=0.7)
 
 # to generate an array
 generate_button = tk.Button(upper_frame, text="Generate\nArray",
-                            command=generate_arrays, bg="#036D19",
-                            font=("Plantagenet Cherokee", 16))
-generate_button.place(relx=0.88, rely=0.2, width=64, height=64)
+                            command=generate_arrays,
+                            font=("Plantagenet Cherokee", 16), bg="#EDAFB8")
+generate_button.place(relx=0.88, rely=0.15, relwidth=0.1, relheight=0.7)
 
 
 # widgets on the right frame
@@ -173,16 +174,60 @@ algo_list.insert(2, "Bubble Sort")
 algo_list.insert(3, "Quick Sort")
 algo_list.insert(4, "Insertion Sort")
 algo_list.insert(5, "Heap Sort")
-algo_list.place(relx=0.1, rely=0.20, height=120, width=100)
+algo_list.place(relx=0.1, rely=0.20, relheight=0.4, relwidth=0.8)
 
 speed_scale = tk.Scale(right_frame, from_=0.1, to=2.0,
                        orient="horizontal", label="Animation Speed(s)", troughcolor="#4A5759", bg="#EDAFB8",
                        font=("Plantagenet Cherokee", 16), resolution=0.2)
-speed_scale.place(relx=0.1, rely=0.65, width=150, height=60)
+speed_scale.place(relx=0.1, rely=0.65, relwidth=0.8, relheight=0.25)
 
 start_button = tk.Button(right_frame, text="Start Sorting",
                          command=start_algorithm,
                          font=("Plantagenet Cherokee", 16), bg="#EDAFB8")
-start_button.place(relx=0.1, rely=0.9, relwidth=0.8)
+start_button.place(relx=0.1, rely=0.91, relwidth=0.8)
 
 root.mainloop()
+
+# code for dynamic text size from stakeoverflow
+# from tkinter import font
+# import tkinter as tk
+#
+# https://stackoverflow.com/questions/24440541/python-tkinter-expanding-fontsize-dynamically-to-fill-frame?rq=1
+# class SimpleGUIExample:
+#     def __init__(self, master):
+#         self.master = master
+#         self.master.title("A simple Label")
+#         self.master.bind('<Configure>', self.resize)
+#
+#         self.label_font = font.Font(self.master, family='Arial', size=12, weight='bold')
+#
+#         self.label = tk.Label(self.master, text="Simple Label Resizing!")
+#         self.label.config(font=self.label_font)
+#         self.label.pack(fill=tk.BOTH, expand=tk.YES)
+#
+#         self.close_button = tk.Button(self.master, text="Close", command=master.quit)
+#         self.close_button.pack()
+#
+#     def resize(self, event):
+#         height = self.label.winfo_height()
+#         width = self.label.winfo_width()
+#         height = height // 2
+#         print('height %s' % height)
+#         print('width %s' % width)
+#         if height < 10 or width < 200:
+#             height = 10
+#         elif width < 400 and height > 20:
+#             height = 20
+#         elif width < 600 and height > 30:
+#             height = 30
+#         else:
+#             height = 40
+#         print('height %s' % height)
+#
+#         self.label_font['size'] = height
+#         print(self.label_font.actual())
+#
+#
+# root = tk.Tk()
+# simple_gui = SimpleGUIExample(root)
+# root.mainloop()
